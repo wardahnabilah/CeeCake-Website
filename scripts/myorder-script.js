891ef6e284b3e4a359e39039cf8c20dc8b33809c
wardahnabilah <wardahnabilah33@gmail.com>
2022-12-11 06:53:57 +0700

    Add invalid input notification

--- scripts/myorder-script.js
+++ scripts/myorder-script.js
@@ -23,7 +23,46 @@ function buttonState() {
     }
 }
 
-// Add eventListener to all inputs
+
+/** Show notification when input value invalid **/
+
+let nameInput = document.querySelector("#receiver-name");
+let addressInput = document.querySelector("#address");
+let phoneInput = document.querySelector("#phone-number");
+
+let nameNotification = document.querySelector("#name-notification");
+let addressNotification = document.querySelector("#address-notification");
+let phoneNotification = document.querySelector("#phone-notification");
+
+function showNotification() {
+    // Receiver name input
+    if(!nameInput.validity.valid) {
+        nameNotification.classList.add("show");
+    }
+    else {
+        nameNotification.classList.remove("show");
+    }
+
+    // Address input
+    if(!addressInput.validity.valid) {
+        addressNotification.classList.add("show");
+    }
+    else {
+        addressNotification.classList.remove("show");
+    }
+
+    // Phone number input
+    if(!phoneInput.validity.valid) {
+        phoneNotification.classList.add("show");
+    }
+    else {
+        phoneNotification.classList.remove("show");
+    }
+}
+
+
+/** Add eventListener to all inputs **/
 for(let i=0; i<input.length; i++) {
     input[i].addEventListener("input", buttonState);
+    input[i].addEventListener("input", showNotification);
 }
